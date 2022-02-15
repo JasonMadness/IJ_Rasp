b3d2f4f88051d459857fcd88d2910d7ad583eef8
JasonMadness <tobc_madness@mail.ru>
2022-02-15 17:21:54 +0300

    Update Weapon_v3.cs

--- Weapon_v3.cs
+++ Weapon_v3.cs
@@ -1,61 +1,52 @@
-﻿using System;
-
-class Weapon
+﻿public class Weapon
 {
-    private int _damage;
-    private int _minDamage = 1;
-    private int _maxDamage = 5;
-    private int _bullets;
-    private int _maxBullets = 30;
-
     private const int BulletsPerShot = 1;
+
+    private int _damage;
+    private int _bullets;
 
     public Weapon(int damage, int bullets)
     {
-        if (damage < _minDamage || damage > _maxDamage)
+        if (damage < 0)
             throw new ArgumentOutOfRangeException(nameof(damage));
 
-        if (bullets < 0 || bullets > _maxBullets)
+        if (bullets < 0)
             throw new ArgumentOutOfRangeException(nameof(bullets));
 
         _damage = damage;
         _bullets = bullets;
     }
 
-    public void TryFire(Player player)
+    public void Fire(Player player)
     {
-        if (CanFire())
+        if (CanFire() == true)
         {
-            Fire(player);
+            player.TakeDamage(_damage);
+            _bullets -= BulletsPerShot;
         }
     }
 
     private bool CanFire()
     {
-        return _bullets > BulletsPerShot;
-    }
-
-    private void Fire(Player player)
-    {
-        player.TakeDamage(_damage);
-        _bullets -= BulletsPerShot;
+        return _bullets >= BulletsPerShot;
     }
 }
 
-class Player
+public class Player
 {
     private int _health;
-    private int _maxHealth = 30;
-
-    public bool IsAlive { get; private set; }
 
     public Player(int health)
     {
-        if (health < 0 || health > _maxHealth)
+        if (health < 0)
             throw new ArgumentOutOfRangeException(nameof(health));
 
         _health = health;
-        IsAlive = true;
+    }
+
+    public bool IsAlive()
+    {
+        return _health > 0;
     }
 
     public void TakeDamage(int damage)
@@ -64,16 +55,10 @@ class Player
             throw new ArgumentOutOfRangeException(nameof(damage));
 
         _health -= damage;
-        TryDie();
-    }
-
-    private void TryDie()
-    {
-        IsAlive = _health > 0;
     }
 }
 
-class Bot
+public class Bot
 {
     private readonly Weapon _weapon;
 
@@ -81,13 +66,13 @@ class Bot
     {
         if (weapon == null)
             throw new ArgumentNullException(nameof(weapon));
-        else
-            _weapon = weapon;
+
+        _weapon = weapon;
     }
 
     public void OnSeePlayer(Player player)
     {
-        if (player.IsAlive == true)
-            _weapon.TryFire(player);
+        if (player.IsAlive() == true)
+            _weapon.Fire(player);
     }
 }
